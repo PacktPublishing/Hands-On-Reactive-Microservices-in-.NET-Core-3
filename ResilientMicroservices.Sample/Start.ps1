21573fa65b3627374e4437f2ba5f46837a3a86b3
Prajeesh Prathap <prajeesh.prathap@gmail.com>
2019-01-23 07:51:30 +0100

    added support for EFK

--- ResilientMicroservices.Sample/Start.ps1
+++ ResilientMicroservices.Sample/Start.ps1
@@ -1,1 +1,1 @@
-docker-compose  -f "docker-compose.yml" -f "docker-compose.override.yml" -p dockercomposelocalrun up -d
+docker-compose  -f "docker-compose.yml" -f "docker-compose.override.yml" -f "docker-compose.efk.yml" -p dockercomposelocalrun up -d
